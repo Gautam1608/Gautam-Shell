96cd3e08b54606c5ab23bb2f1b68527323ec299f
Gautam1608 <pgautam7121@gmail.com>
2026-06-28 10:49:03 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,12 +2,12 @@ import sys
 
 
 def main():
-    command_list=[]
+    command_list=["exit"]
     while True:
         sys.stdout.write("$ ")
         command = input()
         if command.split(" ")[0] in command_list:
-            ...
+            sys.exit()
         else:
             print(f"{command.split(" ")[0]}: command not found")
 
