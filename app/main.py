b680c538a2caff34fe555f1578cc86c4b59d83a1
Gautam1608 <pgautam7121@gmail.com>
2026-06-28 10:58:03 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,14 +2,20 @@ import sys
 
 
 def main():
-    command_list=["exit"]
+    command_list=["exit", "echo"]
     while True:
         sys.stdout.write("$ ")
-        command = input()
-        if command.split(" ")[0] in command_list:
-            sys.exit()
+        full_command = input()
+        command_args=full_command.split(" ")[1:]
+        command = full_command.split(" ")[0]
+        if command in command_list:
+            if command == "exit":
+                sys.exit()
+            elif command == "echo":
+                [sys.stdout.write(args+" ") for args in command_args]
+                print()
         else:
-            print(f"{command.split(" ")[0]}: command not found")
+            print(f"{command}: command not found")
 
 
 
