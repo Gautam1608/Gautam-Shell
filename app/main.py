a1f89f61faefedc4f4e91f9d772a06fc586bc931
Gautam1608 <pgautam7121@gmail.com>
2026-06-28 11:06:27 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,18 +2,23 @@ import sys
 
 
 def main():
-    command_list=["exit", "echo"]
+    builtin=["exit", "echo", "type"]
     while True:
         sys.stdout.write("$ ")
         full_command = input()
         command_args=full_command.split(" ")[1:]
         command = full_command.split(" ")[0]
-        if command in command_list:
+        if command in builtin:
             if command == "exit":
                 sys.exit()
             elif command == "echo":
                 [sys.stdout.write(args+" ") for args in command_args]
                 print()
+            elif command == "type":
+                if command_args[0] in builtin:
+                    print(f"{command_args[0]} is a shell builtin")
+                else:
+                    print(f"{command_args[0]}: not found")
         else:
             print(f"{command}: command not found")
 
