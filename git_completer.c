f56cde0f8d8dfd3a08bf5dfa912e5361d3f6f706
Gautam1608 <pgautam7121@gmail.com>
2026-07-02 11:58:41 +0530

    codecrafters submit [skip ci]

---
+++ git_completer.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main(int argc, char *argv[]) {
+    if (argc < 4) {
+        return 1;
+    }
+
+    char *command = argv[1];      // e.g., "git"
+    char *current = argv[2];      // current word being completed
+    char *previous = argv[3];     // previous word
+
+    // Get environment variables
+    char *comp_line = getenv("COMP_LINE");
+    char *comp_point_str = getenv("COMP_POINT");
+    int comp_point = comp_point_str ? atoi(comp_point_str) : 0;
+
+    // Simple git completion logic
+    if (strcmp(command, "git") == 0) {
+        if (strcmp(previous, "remote") == 0) {
+            if (strstr(current, "set") == current) {
+                printf("set-url\n");
+            } else {
+                printf("add\n");
+                printf("remove\n");
+                printf("set-url\n");
+                printf("show\n");
+            }
+        }
+        else if (strcmp(current, "remote") == 0) {
+            printf("remote\n");
+        }
+    }
+
+    return 0;
+}   
