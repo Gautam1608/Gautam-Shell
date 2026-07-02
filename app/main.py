c5953d831b0bda7614db3922e872a8c573ba0049
Gautam1608 <pgautam7121@gmail.com>
2026-07-02 09:58:46 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -14,6 +14,7 @@ import logging
 # logger = logging.getLogger(__name__)
 try:
     import readline
+    readline.set_completer_delims(" \t\n")
 except ImportError:
     try:
         from pyreadline3 import Readline
